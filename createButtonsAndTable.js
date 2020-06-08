63b96763c66eaac589caf3d12965e9ffff5badd0
Alastair Brown <alastair.brown@BRI51135L.home>
2020-06-08 17:07:23 +0100

    Dynamically create teh buttons based on the data

--- createButtonsAndTable.js
+++ createButtonsAndTable.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     // Initialise first time use
+    initPeopleButtons();
     initTable("Robbie");
 
     $(".btn").click(function (event){
@@ -21,4 +22,13 @@ $(document).ready(function() {
       } );
       $('#Title').text("The Top 10 Albums in the world ever according to " + key + "!");
     };
+
+    function initPeopleButtons(){
+      var buttonPre='<div class="btn-group mr-1" role="group" ><button type="button" class="btn btn-primary">'
+      var buttonPost='</button></div>'
+      // For every person in the data add a button with text that's the key into the data.
+      for (var person in dataAll) {
+        $('#peopleButtons').append(buttonPre + person + buttonPost);
+      }
+    }
 } );
